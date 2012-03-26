4f5ca920e791db111c3a6d0cf55971f9126eb8bc
Josep Jaume <josepjaume@gmail.com>
2012-03-26 16:54:33 +0200

    Bugfix release

--- lib/empresaula-assets/railtie.rb
+++ lib/empresaula-assets/railtie.rb
@@ -2,4 +2,5 @@ require 'rails'
 require 'compass-rails'
 
 class Empresaula::Assets::Railtie < Rails::Engine
+  config.assets.precompile += ['empresaula/*.png', 'empresaula/*.jpg', 'empresaula/*.gif']
 end
