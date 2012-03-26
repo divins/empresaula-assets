d4ecf233abaadd28780f2e4855c0d6fb02e65ad1
Josep Jaume <josepjaume@gmail.com>
2012-03-26 17:57:09 +0200

    Add docs

--- app/helpers/empresaula_helper.rb
+++ app/helpers/empresaula_helper.rb
@@ -1,4 +1,6 @@
 module EmpresaulaHelper
+  # Use it in your views to automatically add the favicon header tag
+  #
   def favicon
     tag :link, rel: 'shortcut icon', href: asset_path('favicon.png')
   end
