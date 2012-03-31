e01487654e5b395a73d7a3a3120567579a15204b
Josep Jaume <josepjaume@gmail.com>
2012-03-31 13:46:13 +0200

    Add a logo helper

--- app/helpers/empresaula_helper.rb
+++ app/helpers/empresaula_helper.rb
@@ -4,4 +4,15 @@ module EmpresaulaHelper
   def favicon
     tag :link, rel: 'shortcut icon', href: asset_path('empresaula/favicon.png')
   end
+
+  # Puts the logo
+  def logo(options= {})
+    options[:url] ||= '/'
+    options[:title] ||= 'Empresaula'
+    options[:color] ||= true
+    img = options[:color] ? 'empresaula/logo-color.png' : 'empresaula/logo.png'
+    content_tag :a, href: options[:url], title: options[:title] do
+      image_tag img, alt: options[:title], size: '328x72'
+    end
+  end
 end
